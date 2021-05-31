def57d6da53b943eb6bb207a3854774fab3603cf
DaviLevi <davide.sgalambro@gmail.com>
2021-05-31 15:19:18 -0300

    0.0.0 Setup Projeto

---
+++ src/main/java/br/com/zup/ot5/fase4/criacao_proposta/CriacaoPropostaApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.ot5.fase4.criacao_proposta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriacaoPropostaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CriacaoPropostaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/ot5/fase4/criacao_proposta/CriacaoPropostaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.ot5.fase4.criacao_proposta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriacaoPropostaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
