2c02b12111bb4035e62105b7dae75f49c524857e
DaviLevi <davide.sgalambro@gmail.com>
2021-06-07 10:29:45 -0300

    0.5.0 Login via senha

---
+++ src/main/java/br/com/zup/ot5/fase4/criacao_proposta/autenticacao/ConfiguracaoSeguranca.java
@@ -0,0 +1,30 @@
+package br.com.zup.ot5.fase4.criacao_proposta.autenticacao;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+public class ConfiguracaoSeguranca extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests(authorizeRequests ->
+                        authorizeRequests
+                                .antMatchers(HttpMethod.GET, "/propostas/**").hasAuthority("SCOPE_propostas:leitura")
+                                .antMatchers(HttpMethod.GET, "/cartoes/**").hasAuthority("SCOPE_cartoes:leitura")
+                                .antMatchers(HttpMethod.POST, "/cartoes/**").hasAuthority("SCOPE_cartoes:escrita")
+                                .antMatchers(HttpMethod.POST, "/propostas/**").hasAuthority("SCOPE_propostas:escrita")
+                                .anyRequest().authenticated()
+                )
+            .sessionManagement()
+            	.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+    }
+
+}
+
