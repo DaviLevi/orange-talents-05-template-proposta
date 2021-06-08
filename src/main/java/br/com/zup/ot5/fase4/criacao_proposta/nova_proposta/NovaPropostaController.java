d587574cfb8a06a56403c51fdaf1a4d841ac7e42
DaviLevi <davide.sgalambro@gmail.com>
2021-06-08 02:36:55 -0300

    0.6.5 Como Saber Tudo Funcionando Corretamente

--- src/main/java/br/com/zup/ot5/fase4/criacao_proposta/nova_proposta/NovaPropostaController.java
+++ src/main/java/br/com/zup/ot5/fase4/criacao_proposta/nova_proposta/NovaPropostaController.java
@@ -1,13 +1,18 @@
 package br.com.zup.ot5.fase4.criacao_proposta.nova_proposta;
 
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import javax.annotation.PostConstruct;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +20,10 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import br.com.zup.ot5.fase4.criacao_proposta.dominio.Proposta;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Tag;
+import io.micrometer.core.instrument.Timer;
 
 @RestController
 @RequestMapping("/propostas")
@@ -23,15 +32,36 @@ public class NovaPropostaController {
 	@PersistenceContext
 	private EntityManager em;
 	
+	private final MeterRegistry meterRegistry;
+	private Counter contadorPropostasCriadas;
+	private Counter contadorPropostasCriadasComFalha;
+	private Timer timerCriacaoProposta;
+	
 	@Autowired
 	private AnalisadorFinanceiroProposta analisadorFinanceiro;
+	
+	public NovaPropostaController(MeterRegistry meterRegistry) {
+	    this.meterRegistry = meterRegistry;    
+	}
+	
+	@PostConstruct
+	public void inicializaCalculadoresMetricas() {
+		Collection<Tag> tags = new ArrayList<>();
+		this.contadorPropostasCriadas = this.meterRegistry.counter("proposta_criada_com_sucesso", tags);
+	    this.contadorPropostasCriadasComFalha = this.meterRegistry.counter("proposta_criada_com_falha", tags);
+	    this.timerCriacaoProposta = this.meterRegistry.timer("proposta_criada_com_sucesso_timer", tags);
+	}
 	
 	@PostMapping
 	@Transactional
 	public ResponseEntity<?> cadastra(@RequestBody @Valid NovaPropostaRequest novaProposta, UriComponentsBuilder builder) {
 		
-		if(novaProposta.jaExisteUmaPropostaCriadaParaMesmoSolicitante(em)) 
+		final Long istanteInicioRequisicao = System.currentTimeMillis();
+		
+		if(novaProposta.jaExisteUmaPropostaCriadaParaMesmoSolicitante(em)) {
+			this.contadorPropostasCriadasComFalha.increment();
 			return ResponseEntity.unprocessableEntity().build();
+		}
 		
 		Proposta proposta = novaProposta.paraProposta();
 		
@@ -39,10 +69,19 @@ public class NovaPropostaController {
 		
 		AnaliseFinanceiraResponse respostaAnalise = analisadorFinanceiro.analisa(proposta);
 		
-		if(respostaAnalise == null) // serviço indisponivel
-			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-		
+		if(respostaAnalise == null) {  // serviço indisponivel
+			contadorPropostasCriadasComFalha.increment(); // incremento a metrica de propostas criada com falha
+			throw new RuntimeException("Servico indisponivel");
+		}
+			
 		proposta.anexaResultadoAnaliseFinanceira(respostaAnalise.getResultado());
+		
+		contadorPropostasCriadas.increment(); // incremento a metrica de propostas criada com sucesso
+		
+		final Long istanteFimRequisicao = System.currentTimeMillis();
+		final Long duracaoRequisicaoEmMilisegundos = istanteFimRequisicao - istanteInicioRequisicao;
+		
+		this.timerCriacaoProposta.record(Duration.of(duracaoRequisicaoEmMilisegundos, ChronoUnit.MILLIS));
 		
 		return ResponseEntity.created(builder.path("/propostas/{id}").buildAndExpand(proposta.getId()).toUri()).build();
 	}
